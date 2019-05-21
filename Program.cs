d187938c88cec6bdfa27f44e6451155202307d79
gstas <gstas07@gmail.com>
2019-05-21 10:18:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            task13();
+            /*task14();
+            task37();
+            task38();
+            task44();*/            
+        }
+
+        private static void task13()
+        {
+        }
+            
+    }
+}
